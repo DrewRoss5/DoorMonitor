a887eb94fcda4c6d16e7925a0195540f43b65691
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-07-14 23:32:38 -0600

    Add logging capabilities to client app

--- src/client/main.py
+++ src/client/main.py
@@ -1,14 +1,23 @@
 import sys
+import logging
 
 from plyer import notification
 from sensorServer import SensorServer
 
 
 def __main__():
-    print('Room name: ', end='')
-    room_name = input()
-    print('Server IP: ', end='')
-    ip_addr = input()
+    logging.basicConfig()
+    room_name = input('Room name: ')
+    ip_addr = input('Server Address: ')
+    log_file = input('Log File (leave blank to disable logging): ')
+       # configure the logger
+    log_active = True
+    if log_file.strip() == '':
+        print('Logging disabled')
+        log_active = False
+    else:
+        logging.basicConfig(filename=log_file, filemode='a+', format='%(asctime)s: %(message)s')
+    # configure the sensor
     sensor = None
     try:
         sensor = SensorServer(ip_addr)
@@ -18,18 +27,25 @@ def __main__():
     except OSError as e:
         print(f'Invalid IP address ({e})')
         sys.exit(1)
+    # await a message from the door monitor
     print('Connected')
     sensor_enabled = True
     while sensor_enabled:
         message = sensor.await_message()
         match message:
             case SensorServer.MOTION_DETECTED:
-                notification.notify(title='Door Monitor', message=f'Activity detected {room_name}!', timeout=10)
+                notification.notify(title='Door Monitor', message=f'Activity detected in {room_name}!', timeout=10)
+                if log_active:
+                    logging.info(f'{room_name}: Activity Detected')
             case SensorServer.DISCONNECT:
                 notification.notify(title='Door Monitor', message=f'Sensor for {room_name} has been disabled', timeout=10)
+                if log_active:
+                    logging.info(f'{room_name}: Sensor Disconnected')
                 sys.exit(0)
             case None:
                 notification.notify(title='Door Monitor', message=f'Sensor for {room_name} has failed', timeout=10)
+                if log_active:
+                    logging.info(f'{room_name}: Sensor Failure')
                 sys.exit(1)
 
 if __name__ == '__main__':
